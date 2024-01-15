77c65633e453f3916f5c8092caf367748b29f55b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-15 10:30:27 +0000

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,25 +4,27 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ["latin"],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ],
-  variable: '--font-inter',
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
 
-const spaceGrotesk = Space_Grotesk({ 
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
-  weight: ['300', '400', '500', '600', '700' ],
-  variable: '--font-spaceGrotesk',
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A community-driven platform for asking and answering programming questions. Get help and share your knowledge, and collaborate with other developers from around the world.  Explore topics in web development, mobile app development, algorithms, data structures and more.",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help and share your knowledge, and collaborate with other developers from around the world.  Explore topics in web development, mobile app development, algorithms, data structures and more.",
   icons: {
-    icon: '/assets/images/sit-logo.svg'
-  }
+    icon: "/assets/images/sit-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -31,17 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500',
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
