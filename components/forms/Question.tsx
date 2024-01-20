1747df48cdb0700d78186bcffabbbeaec58fc667
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-20 10:53:45 +0000

    Add TinyMCE editor to Question component

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,4 +1,6 @@
 "use client";
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -17,6 +19,8 @@ import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validations";
 
 const Question = () => {
+  const editorRef = useRef(null);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -54,9 +58,10 @@ const Question = () => {
                 />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Be specific and imagine you&apos;re asking a question to another person.
+                Be specific and imagine you&apos;re asking a question to another
+                person.
               </FormDescription>
-              <FormMessage className="text-red-500"/>
+              <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
@@ -66,15 +71,51 @@ const Question = () => {
           render={({ field }) => (
             <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="paragraph-semibold text-dark400_light800">
-                Detailed explanation of your problem <span className="text-primary-500">*</span>
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* {TODO: Add an Editor component} */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore 
+                  editorRef.current = editor
+                }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist",
+                    content_style:
+                      "body { font-family:Inter; font-size:16px }",
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Introduce the problem and expand on what you put in the title.  Minimum 20 characters.
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
               </FormDescription>
-              <FormMessage className="text-red-500"/>
+              <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
@@ -94,9 +135,10 @@ const Question = () => {
                 />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Add up to 3 tags to describe what your question is about.  You need to press enter after each tag.
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter after each tag.
               </FormDescription>
-              <FormMessage className="text-red-500"/>
+              <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
