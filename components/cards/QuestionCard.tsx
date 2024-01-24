afe6849979e8a7c629616ba4b1d7d14ef9ab06a9
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-24 21:06:31 +0000

    Add new page for editing questions and implement edit and delete actions

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import RenderTag from "@/components/shared/RenderTag";
 import Metric from "@/components/shared/Metric";
 import { getTimestamp, formatAndDivideNumber } from "@/lib/utils";
+import { SignedIn } from "@clerk/nextjs";
+import EditDeleteAction from "../shared/EditDeleteAction";
 
 interface QuestionProps {
   _id: string;
@@ -20,9 +22,11 @@ interface QuestionProps {
   view: number;
   answers: Array<object>;
   createdAt: Date;
+  clerkId?: string;
 }
 
 const QuestionCard = ({
+  clerkId,
   _id,
   title,
   tags,
@@ -32,6 +36,8 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionProps) => {
+  const showActionButtons = clerkId && clerkId === author.clerkId;
+
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -44,7 +50,14 @@ const QuestionCard = ({
           </Link>
         </div>
 
-        {/* {if signed in add edit delete actions} */}
+      <SignedIn>
+        {showActionButtons && (
+          <EditDeleteAction 
+            type="question"
+            itemId={JSON.stringify(_id)}
+          />
+          )}
+      </SignedIn>
       </div>
 
     <div className="mt-3.5 flex flex-wrap gap-2">
--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,48 +1,54 @@
-"use server"
+"use server";
 
 import Answer from "@/database/answer.model";
 import { connectToDatabase } from "../mongoose";
-import { AnswerVoteParams, CreateAnswerParams, GetAnswersParams } from "./shared.types"
+import {
+  AnswerVoteParams,
+  CreateAnswerParams,
+  DeleteAnswerParams,
+  GetAnswersParams,
+} from "./shared.types";
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
+import Interaction from "@/database/interaction.model";
 
 export async function createAnswer(params: CreateAnswerParams) {
-    try {
-        connectToDatabase();
+  try {
+    connectToDatabase();
 
-        const {content, author, question, path} = params;
-        
-        const newAnswer = await Answer.create({
-            content,
-            author,
-            question
-        });
+    const { content, author, question, path } = params;
 
-        await Question.findByIdAndUpdate(question, {
-            $push: { answers: newAnswer._id }
-        });
+    const newAnswer = await Answer.create({
+      content,
+      author,
+      question,
+    });
 
-        revalidatePath(path);
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+    await Question.findByIdAndUpdate(question, {
+      $push: { answers: newAnswer._id },
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 export async function getAnswers(params: GetAnswersParams) {
-    try {
-        connectToDatabase();
+  try {
+    connectToDatabase();
 
-        const { questionId } = params;
-        const answers = await Answer.find({ question: questionId })
-            .populate('author', "_id clerkId name picture")
-            .sort({ createdAt: -1 });
+    const { questionId } = params;
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
 
-        return { answers}
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
 export async function upvoteAnswer(params: AnswerVoteParams) {
@@ -105,6 +111,32 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
     if (!answer) {
       throw new Error("Answer not found");
     }
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { answerId, path } = params;
+
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) {
+      throw new Error("Answer not found");
+    }
+
+    await answer.deleteOne({ _id: answerId });
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } }
+    );
+    await Interaction.deleteMany({ answers: answerId });
 
     revalidatePath(path);
   } catch (error) {
