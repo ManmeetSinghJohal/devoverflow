e97e96121b68a67ab8f85a83579346dd7be39c43
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-25 11:54:01 +0000

    Update search functionality in Home, Collection, and Community pages

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import UserCard from "@/components/cards/UserCard";
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { UserFilters } from "@/constants/filters";
-import Filter from "@/components/shared/Filter";
 import { getAllUsers } from "@/lib/actions/user.action";
-import UserCard from "@/components/cards/UserCard";
+import { SearchParamsProps } from "@/types";
 
-const Page = async () => {
-  const result = await getAllUsers({});
+const Page = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllUsers({
+    searchQuery: searchParams.q,
+  });
 
   return (
     <>
@@ -29,12 +32,12 @@ const Page = async () => {
 
       <section className="mt-12 flex flex-wrap gap-4">
         {result.users.length > 0 ? (
-          result.users.map((user) => <UserCard key={user._id} user={user}/>)
+          result.users.map((user) => <UserCard key={user._id} user={user} />)
         ) : (
           <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
             <p>No users yet</p>
             <Link href="/sign-up" className="mt-2 font-bold text-accent-blue">
-                Join to be the first
+              Join to be the first
             </Link>
           </div>
         )}
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,17 +3,36 @@
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import Tag from "@/database/tag.model";
-import { CreateQuestionParams, DeleteQuestionParams, EditQuestionParams, GetQuestionByIdParams, GetQuestionsParams, QuestionVoteParams } from "./shared.types";
+import {
+  CreateQuestionParams,
+  DeleteQuestionParams,
+  EditQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+  QuestionVoteParams,
+} from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import Answer from "@/database/answer.model";
 import Interaction from "@/database/interaction.model";
+import { FilterQuery } from "mongoose";
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const questions = await Question.find({})
+    const { searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { title: { $regex: new RegExp(searchQuery, "i") } },
+        { content: { $regex: new RegExp(searchQuery, "i") } },
+      ];
+    }
+
+    const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
       .sort({ createdAt: -1 });
@@ -60,12 +79,16 @@ export async function getQuestionById(params: GetQuestionByIdParams) {
   try {
     connectToDatabase();
 
-    const {questionId} = params;
+    const { questionId } = params;
     const question = await Question.findById(questionId)
-      .populate({ path: "tags", model: Tag, select: ' _id name' })
-      .populate({ path: "author", model: User, select: ' _id clerkId name picture' })
+      .populate({ path: "tags", model: Tag, select: " _id name" })
+      .populate({
+        path: "author",
+        model: User,
+        select: " _id clerkId name picture",
+      });
 
-      return question;
+    return question;
   } catch (error) {
     console.log(error);
     throw error;
@@ -80,22 +103,26 @@ export async function upvoteQuestion(params: QuestionVoteParams) {
 
     let updateQuery = {};
 
-    if(hasupVoted) {
+    if (hasupVoted) {
       updateQuery = { $pull: { upvotes: userId } };
     } else if (hasdownVoted) {
-      updateQuery = { $pull: { downvotes: userId }, $push: { upvotes: userId } };
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
     } else {
       updateQuery = { $addToSet: { upvotes: userId } };
     }
 
-    const question = await Question.findByIdAndUpdate(questionId, updateQuery, { new: true })
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
 
-    if(!question) {
-      throw new Error('Question not found');
+    if (!question) {
+      throw new Error("Question not found");
     }
 
     revalidatePath(path);
-
   } catch (error) {
     console.log(error);
     throw error;
@@ -145,7 +172,10 @@ export async function deleteQuestion(params: DeleteQuestionParams) {
     await Question.deleteOne({ _id: questionId });
     await Answer.deleteMany({ question: questionId });
     await Interaction.deleteMany({ question: questionId });
-    await Tag.updateMany({ questions: questionId }, { $pull: { questions: questionId } });
+    await Tag.updateMany(
+      { questions: questionId },
+      { $pull: { questions: questionId } }
+    );
 
     revalidatePath(path);
   } catch (error) {
@@ -181,9 +211,9 @@ export async function getHotQuestions() {
   try {
     connectToDatabase();
 
-   const hotQuestions = await Question.find({})
-    .sort({views: -1, upvotes: -1})
-    .limit(5)
+    const hotQuestions = await Question.find({})
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
 
     return hotQuestions;
   } catch (error) {
