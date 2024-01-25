48c4d580384dc55b3737d98039ab469934d307a2
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-25 17:19:35 +0000

    Add filter functionality to community page and update user sorting

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -9,6 +9,8 @@ import { SearchParamsProps } from "@/types";
 const Page = async ({ searchParams }: SearchParamsProps) => {
   const result = await getAllUsers({
     searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    
   });
 
   return (
