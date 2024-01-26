003aa3fdd1285617d7bb1ee31910e50d71c9d184
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-01-26 16:04:42 +0000

    Add AI answer generation functionality

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -27,6 +27,7 @@ interface Props {
 const Answer = ({ question, questionId, authorId }: Props) => {
   const pathname = usePathname();
   const [isSubmiting, setIsSubmiting] = useState(false);
+  const [isSubmitingAI, setIsSubmitingAI] = useState(false);
   const { mode } = useTheme();
   const editorRef = useRef(null);
   const form = useForm<z.infer<typeof AnswersSchema>>({
@@ -61,6 +62,28 @@ const Answer = ({ question, questionId, authorId }: Props) => {
     }
   };
 
+  const generateAIAnswer = async () => {
+    if(!authorId) return;
+
+    setIsSubmitingAI(true);
+    
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`, {
+        method: 'POST',
+        body: JSON.stringify({question})
+      });
+
+      const aiAnswer = await response.json();
+
+      alert(aiAnswer.reply);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsSubmitingAI(false);
+    }
+  };
+
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -70,7 +93,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-          onClick={() => {}}
+          onClick={generateAIAnswer}
         >
           <Image
             src="/assets/icons/stars.svg"
@@ -79,7 +102,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             height={12}
             className="object-contain"
           />
-          Generate an AI Answer
+          Generate AI Answer
         </Button>
       </div>
 
