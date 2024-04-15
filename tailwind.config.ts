8f611dc7eae548e97893ea8ec60f3af65edfc00c
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-15 18:28:56 +0100

    Update tailwind.config.ts with color and style changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     },
     extend: {
       colors: {
-        primary: {
+        primary1: {
           500: "#FF7000",
           100: "#FFF1E6",
         },
@@ -38,6 +38,39 @@ const config: Config = {
           400: "#858EAD",
         },
         "accent-blue": "#1DA1F2",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       fontFamily: {
         inter: ["var(--font-inter)"],
